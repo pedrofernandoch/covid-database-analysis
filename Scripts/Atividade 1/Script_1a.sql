5d14a0d90ee8661eb1a8c0b77756ceb345dfcb00
Vinicius Carvalho <vgdcarvalho@gmail.com>
2021-07-10 17:46:02 -0300

    Adicionando query para buscar 20 primeiros resultados e para analise

--- Scripts/Atividade 1/Script_1a.sql
+++ Scripts/Atividade 1/Script_1a.sql
@@ -1,3 +1,27 @@
+/*
+ 	Projeto Final de Labortário de Banco de Dados (SCC0541 2021/1)
+ 		Pedro Fernando Christofoletti dos Santos - 11218560
+   		Vinícius Gonçalves de Carvalho - 8517157
+ */
+
+-- ATIVIDADE 1a
+
+-- Query original, dada no enunciado da Atividade 1
+SELECT * FROM exames ex
+JOIN pacientes p ON p.id_paciente = ex.id_paciente
+JOIN desfechos d ON p.id_paciente = d.id_paciente
+WHERE ex.de_origem = 'Unidades de Internação';
+
+-- Query original, alterada para trazer apenas os 20 primeiros resultados, organizadas em ordem crescente por id_exame
+SELECT * FROM exames ex
+JOIN pacientes p ON p.id_paciente = ex.id_paciente
+JOIN desfechos d ON p.id_paciente = d.id_paciente
+WHERE ex.de_origem = 'Unidades de Internação'
+ORDER BY id_exame 
+LIMIT 20;
+
+-- Análise da query original, contendo os tempos
+EXPLAIN ANALYZE
 SELECT * FROM exames ex
 JOIN pacientes p ON p.id_paciente = ex.id_paciente
 JOIN desfechos d ON p.id_paciente = d.id_paciente
